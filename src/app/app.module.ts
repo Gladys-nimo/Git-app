77d6a10fce7dfe4f49f23ade2d38cadb05781325
Gladys-nimo <wkanyora10@gmail.com>
2020-11-16 09:08:35 +0300

    added some classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { FormsModule } from '@angular/forms';
+import { GitService } from './git.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitFormComponent } from './git-form/git-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
