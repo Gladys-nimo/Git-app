7c73a759c2054c986441ddbe1bc4305876c9c1a5
Gladys-nimo <wkanyora10@gmail.com>
2020-11-19 10:55:52 +0300

    closed a tag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { GitFormComponent } from './git-form/git-form.component';
 import { AboutComponent } from './about/about.component';
-// import { SearchComponent } from './search/search.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'form', component:GitFormComponent
+  { path: 'form', component:GitFormComponent},
   { path: 'about', component:AboutComponent},
 ];
 
