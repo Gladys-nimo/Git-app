230dd0a06f6b42d47f405edc03c3eb200310c74b
Gladys-nimo <wkanyora10@gmail.com>
2020-11-19 10:02:06 +0300

    added some changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,16 +1,15 @@
 import { User } from './../user';
 import { NgForm } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
 import { GitService } from './../git.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
    
 user:User;
@@ -23,16 +22,16 @@ hideInput:boolean;
     this.gitService = gitService;
   }
 
-  @Output() toggleBack = new EventEmitter();
+  // @Output() toggleBack = new EventEmitter();
 
-  goBack(){
-    this.hideInput = true;
-    this.toggleBack.emit(this.hideInput);
-  }
+  // goBack(){
+  //   this.hideInput = true;
+  //   this.toggleBack.emit(this.hideInput);
+  // }
 
   ngOnInit() {
     this.user = this.gitService.user;
-    // this.repoDetails = this.gitService
+    this.repoDetails = this.gitService.repoData
   }
 
 }
