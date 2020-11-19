c518567f28d4a3d7bd45bceb3e9d03b5c9a4399a
Gladys-nimo <wkanyora10@gmail.com>
2020-11-19 12:16:09 +0300

    added some content in the new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { GitFormComponent } from './git-form/git-form.component';
 import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'form', component:GitFormComponent},
   { path: 'about', component:AboutComponent},
+  { path:'**', component:NotFoundComponent}
 ];
 
 @NgModule({
